0309ee47ae88c3fd96f15c78038c09a5db74a28f
Thakur Kharel <kharelthakur19@gmail.com>
2021-03-05 16:31:25 -0600

    adding some sample images and firebase data testing

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,14 +1,24 @@
+import { useScrollToTop } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Feed from '../components/Feed';
+import firestore from "../firebase/Firebase";
 
 export default function HomeFeed() {
+  var posts = firestore.ref('users');
+  posts.on('value',(snapshot) => {
+    const data = snapshot.val();
+    console.log(data)
+  })
   return (
     <View style={styles.container}>
      <Feed/>
     </View>
   );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
