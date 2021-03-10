c2691de42bef1821680c85d1e24db9d6d21e31e7
Thakur Kharel <kharelthakur19@gmail.com>
2021-03-09 22:45:38 -0600

    working tap features and feed posts loading

--- components/Feed.js
+++ components/Feed.js
@@ -1,82 +1,80 @@
-import React from 'react';
-import { StyleSheet, Text, View, FlatList, ActivityIndicator, Image, TouchableOpacity } from 'react-native';
-import firebase from 'firebase';
-import firestore from '../firebase';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import FeedPosts from './FeedPosts';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import firebase from "firebase";
+import firestore from "../firebase";
+import { useState } from "react";
+import { useEffect } from "react";
+import FeedPosts from "./FeedPosts";
 
-async function getShit(){
-  const allPosts = await firestore.collection('posts').get()
-  return allPosts.docs.map(doc => doc.data())
+async function getPosts() {
+  const allPosts = await firestore.collection("posts").get();
+  return allPosts.docs.map((doc) => doc.data());
 }
 
-export default function HomeFeed({contents,onPostRequested}) {
-  const [loading,setLoading] = useState(false)
-  const [allPosts, setAllPosts] = useState([])
+export default function HomeFeed({ contents, onPostRequested }) {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState([]);
 
   useEffect(() => {
-    if(contents){
-      setAllPosts(contents)
-    }else{
+    if (contents) {
+      setAllPosts(contents);
+    } else {
       getAllPosts();
     }
-  },[contents])
+  }, [contents]);
 
   let getAllPosts = () => {
     setLoading(true);
-    var posts = getShit();
-    posts.then( (data) => {
+    var posts = getPosts();
+    posts.then((data) => {
       // console.log(data)
-      setAllPosts(data)
-      setLoading(false)
-    }
-    )
-  }
+      setAllPosts(data);
+      setLoading(false);
+    });
+  };
 
   let onPostPressed = (post) => {
-    onPostRequested(post)
-  }
+    onPostRequested(post);
+  };
 
   let _keyExtractor = (item, index) => item.post_id;
 
-  let renderItem = ({item}) => {
-   return <FeedPosts content={item}/>
-  }
-  
-  function displayedContents(){
+  let renderItem = ({ item }) => {
+    return <FeedPosts content={item} />;
+  };
+
+  function displayedContents() {
     let content = null;
-    if(loading){
+    if (loading) {
       content = <ActivityIndicator size="large" color="#0000ff" />;
-    }else{
-      content = 
-      <TouchableOpacity>
-           <FlatList
-             data = {allPosts}
-             renderItem={renderItem}
-             keyExtractor={_keyExtractor}     
-         />
-      </TouchableOpacity>
-   
+    } else {
+      content = (
+        <FlatList
+          data={allPosts}
+          renderItem={renderItem}
+          keyExtractor={_keyExtractor}
+        />
+      );
     }
     return content;
   }
 
-
-
-
-  return (
-    <View style={styles.container}>
-    {displayedContents()}
-  </View>
-  );
+  return <View style={styles.container}>{displayedContents()}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- components/FeedPosts.js
+++ components/FeedPosts.js
@@ -1,16 +1,63 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
-import firebase from 'firebase';
-import firestore from '../firebase';
+import firebase from "firebase";
+import firestore from "../firebase";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import { useState } from "react/cjs/react.development";
+import { useEffect } from "react";
+import { Card } from "react-native-paper";
 
-async function getShit(user_id){
-    
-    const user = await firestore.collection('users').doc('thakur')
-    console.log("UserData",user)
-  }
 
 export default function FeedPosts({ content, onPostRequested }) {
-    getShit(content.user_id);
-    console.log(content)
-  return <Text>{content.user_id}</Text>;
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const getUserData = async (user) => {
+      const uniqueUser = await user.get();
+      if (!uniqueUser.exists) {
+        console.log("No User Found");
+      } else {
+        setUser(uniqueUser.data())
+      }
+    };
+    getUserData(content.user)
+  },[])
+  function  onPress(params) {
+    console.log(user.first_name)
+  }
+  return (
+   
+      <Card style={styles.container}>
+         <TouchableOpacity onPress={onPress}>
+        <Image source = {{uri:user.profile_pic}} style={styles.image} />
+        </TouchableOpacity>
+        <Text>{user.first_name + " " + user.last_name}</Text>
+        <Text>{content.campaign}</Text>
+        <Text> View all {content.comment_count} comments </Text>
+        <Text>{content.likes}</Text>
+        <Text>{content.comments[0].content}</Text>
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          // onChangeText={text => onChangeText(text)}
+          // value={value}
+        />
+      </Card>
+    
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor: "#ecf0f1",
+    borderRadius: 50,
+    width: 367,
+    height: 364.05,
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+});
